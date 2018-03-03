b0ceb7e4b0b5f3174b4ae091c8a2cf9c392a584b
Jerry Guo <jerrguo@gmail.com>
2018-03-03 01:21:46 -0500

    currency input done

--- app.py
+++ app.py
@@ -2,23 +2,37 @@ from flask import Flask, render_template, request, redirect, jsonify
 from graph.weighted_graph import weighted_graph
 import json
 
+support_currs = ['EUR', 'USD', 'JPY', 'CHF', 'AUD', 'CAD', 'NZD', 'GBP', 'SEK', 'NOK', 'MXN', 'TRY', 'ZAR', 'CNH', 'XAU', 'XAG']
+support_currs_string = ', '.join(support_currs)
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-
-    currs=None
+@app.route('/', methods=['GET','POST'])
+def currencies():
+    currencies = ""
+    temp_list = []
 
     if request.method == 'POST':
-        curr = request.form.getlist('currencies')
-        if len(curr) > 1:
-            currs = weighted_graph(curr)
-            currs = currs.table_dict
+        currencies = request.form['currs']                          # Let currenices equal the string value in textfield
+        currencies = currencies.upper().replace(" ", "")            # Set currencies to capitals and remove whitespace
+        temp_list = currencies.split(',')                           # Temporary list of inputs
+        curr_list = []                                              # List to store valid inputs
 
-    return render_template('test.html', currs=currs)
+        # Detect any non-supported currency values and remove them
+        for currency in temp_list:
+            if currency in support_currs:
+                curr_list.append(currency)
+
+        curr_list = list(set(curr_list))
+        currencies = ', '.join(curr_list)                           # Recompile string of currencies excluding erroneous values
+
+        print('THIS: ' + currencies)
+    else:
+        currencies = ""
+
+    return render_template('home.html', currencies=currencies, support_currs_string=support_currs_string)
 
 
 
 if __name__ == '__main__':
-    app.run(debug='on')
+    app.run(debug = 'True')
