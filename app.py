928d5f7e105c0572fc170a5b5df2e56c30abfb84
Jerry Guo <jerrguo@gmail.com>
2018-03-03 14:03:33 -0500

    graph web app part complete

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect
 from graph.weighted_graph import weighted_graph
-import json
+import time
 
 support_currs = ['EUR', 'USD', 'JPY', 'CHF', 'AUD', 'CAD', 'NZD', 'GBP', 'SEK', 'NOK', 'MXN', 'TRY', 'ZAR', 'CNH', 'XAU', 'XAG']
 support_currs_string = ', '.join(support_currs)
@@ -9,35 +9,47 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def currencies():
-    currencies = ""
+    currencies_string = ""
+    opportunity_string = ""
     temp_list = []
+    opportunites = []
 
     if request.method == 'POST':
-        currencies = request.form['currs']                          # Let currenices equal the string value in textfield
-        currencies = currencies.upper().replace(" ", "")            # Set currencies to capitals and remove whitespace
-        temp_list = currencies.split(',')                           # Temporary list of inputs
-        curr_list = []                                              # List to store valid inputs
+        currencies_string = request.form['currs']                                                   # Let currenices equal the string value in textfield
+        currencies_string = currencies_string.upper().replace(" ", "")                              # Set currencies to capitals and remove whitespace
+        temp_list = currencies_string.split(',')                                                    # Temporary list of inputs
+        curr_list = []                                                                              # List to store valid inputs
 
         # Detect any non-supported currency values and remove them
         for currency in temp_list:
             if currency in support_currs:
                 curr_list.append(currency)
 
-        curr_list = list(set(curr_list))                            # Remove duplicate currencies from list
-        currencies = ', '.join(curr_list)                           # Recompile string of currencies excluding erroneous values
-
-        print('THIS: ' + currencies)
+        curr_list = list(set(curr_list))                                                            # Remove duplicate currencies from list
+        currencies_string = ', '.join(curr_list)                                                    # Recompile string of currencies excluding erroneous values
 
         # Arbitrage Detection Algorithm
-        curr_list = ['USD', 'CAD', 'GBP', 'AUD']                           # TODO TESTING
         if len(curr_list) > 1:
             graph = weighted_graph(curr_list)
-            opportunities = graph.show_arbitrage_opportunities()    # Opportunities stores the arbitrage cycles in list form
+            opportunities = graph.show_arbitrage_opportunities()                                    # Opportunities stores the arbitrage cycles in list form
+            time_epoch = graph.get_time()                                                           # Get timestamp from graph
+            time_string = time.strftime("%Z - %Y/%m/%d, %H:%M:%S", time.localtime(time_epoch))      # Convert to readable time
+
+            if len(opportunites) == 0:
+                opportunity_string = "No arbitrage opportunity (" + time_string + ")"
+
+            else:
+                opp_string = ' -> '.join(opportunites[0])
+                opportunity_string = opp_string + ' at ' + time_string
+
+        else:
+            opportunity_string = "No arbitrage opportunity available with less than two currencies..."
 
     else:
-        currencies = ""
+        opportunity_string = "No arbitrage opportunity available with less than two currencies..."
 
-    return render_template('home.html', currencies=currencies, support_currs_string=support_currs_string)
+
+    return render_template('home.html', currencies=currencies_string, support_currs=support_currs_string, opportunity=opportunity_string)
 
 
 
--- graph/weighted_graph.py
+++ graph/weighted_graph.py
@@ -17,65 +17,74 @@ def create_predecessor_table(curr):
 
 def show_negative_weight_cycle(predecessors, node):
 
+    print ('NEGATIVE CYCLE')
+    print ('PRED: ' + str(predecessors))
+    print ('NODE: ' + node)
+
     arbitrage = [node]                                      # temp list used to display currencies with arbitrage opportunities
     next_node = predecessors[node]
-
-    print('PRED: ' + str(predecessors))
 
     while next_node not in arbitrage:
         arbitrage.append(next_node)
         next_node = predecessors[next_node]
 
     arbitrage.append(next_node)
-    arbitrage = arbitrage[arbitrage.index(next_node):]
+    arbitrage = arbitrage[arbitrage.index(next_node):]      # nodes appended in backwards order of arbitrage
+    arbitrage = arbitrage[::-1]                             # reverse order of list to get arbitrage opportunites in forward order
     return arbitrage
 
 
-############################################        WEIGHTED GRAPH      ############################################
+#####################################################        WEIGHTED GRAPH      #####################################################
 
 class weighted_graph:
 
     def __init__(self, currs):
 
-        self.currencies = currs                                         # list of currencies
-        self.curr_df, self.curr_matrix, self.table_dict = get_data(currs)                # currency data frame (with listings of all edges)
+        self.currencies = currs                                             # list of currencies
+        self.curr_df, self.curr_matrix, self.table_dict = get_data(currs)   # currency data frame (with listings of all edges)
         #print('TESTING WEIGHTED GRAPH')
         #print (self.curr_df)
         #print ('\n')
 
     def get_weight(self, edge):
 
-        edge_rate = self.curr_df.get_value(edge, 'edge_weight')         # edge weights based on NEGATIVE LOG-EXCHANGE RATE
+        edge_rate = self.curr_df.get_value(edge, 'edge_weight')             # edge weights based on NEGATIVE LOG-EXCHANGE RATE
         return edge_rate
+
+    def get_time(self):
+
+        return self.curr_df['timestamp'].iloc[0]                            # return timestamp at first row
 
     def get_default_weight(self, edge):
 
-        edge_rate = self.curr_df.get_value(edge, 'ask')                 # edge weights based on ASK RATE
+        edge_rate = self.curr_df.get_value(edge, 'ask')                     # edge weights based on ASK RATE
         return edge_rate
 
     def bellmanford(self, start):
 
-        updated = True                                                  # keeps track of whether an update was made or not
-        iteration = 1                                                   # keeps track of the iteration number
-        num_currencies = len(self.currencies)                           # number of currencies
-        currency_list = self.currencies                                 # temp list to store currencies
+        updated = True                                                      # keeps track of whether an update was made or not
+        iteration = 1                                                       # keeps track of the iteration number
+        num_currencies = len(self.currencies)                               # number of currencies
+        currency_list = self.currencies                                     # temp list to store currencies
 
         dist_table = create_distance_table(currency_list)
-        dist_table[start] = 0                                           # set up start node
-        pre_table = create_predecessor_table(currency_list)             # predecessor table
+        dist_table[start] = 0                                               # set up start node
+        pre_table = create_predecessor_table(currency_list)                 # predecessor table
 
         # FIRST |V-1| ITERATIONS
         while iteration < num_currencies and updated == True:
             #print (dist_table)             # shortest path table
             #print (pre_table)              # predecessor table
-            updated = False                                             # reset updated for checking if an update occurs later
-            for from_curr in currency_list:                             # First Loop for each currency
-                for to_curr in currency_list:                           # Second Loop for each currency that
-
-                        if dist_table[from_curr] + self.get_weight(from_curr + to_curr) < dist_table[to_curr]:              # if a shorter path is found to THIS node
-                            dist_table[to_curr] = dist_table[from_curr] + self.get_weight(from_curr + to_curr)              # update distance table with shorter path distance
-                            pre_table[from_curr] = to_curr                                                                  # update predecessor table with shorter path node
-                            updated = True                                                                                  # an update occurred so updated = True
+            updated = False                                                 # reset updated for checking if an update occurs later
+            for from_curr in currency_list:                                 # First Loop for each currency
+                for to_curr in currency_list:                               # Second Loop for each currency that
+                    if dist_table[from_curr] + self.get_weight(from_curr + to_curr) < dist_table[to_curr]:              # if a shorter path is found to THIS node
+                        dist_table[to_curr] = dist_table[from_curr] + self.get_weight(from_curr + to_curr)              # update distance table with shorter path distance
+                        pre_table[to_curr] = from_curr                                                                  # update predecessor table with shorter path node
+                        updated = True                                                                                  # an update occurred so updated = True
+                        print ('(1): ' + from_curr + ' TO ' + to_curr)
+                        print (str(dist_table))
+                        print (str(pre_table) + '\n')
 
             iteration = iteration + 1
 
@@ -83,23 +92,16 @@ class weighted_graph:
         #print (pre_table)              # predecessor table
         #print ('\n')
 
-        # FINAL ITERATIONS to detect NEGATIVE WEIGHT CYCLES
+        print ('NEGATIVE DETECTION ALGO')
+
+        # FINAL ITERATION to detect NEGATIVE WEIGHT CYCLES
         if updated == True:
-            iteration = 1
-            while iteration < num_currencies and updated == True:
-                updated = False
-                for from_curr in currency_list:                           # First Loop for each currency
-                    for to_curr in currency_list:                         # Second Loop for each currency that
+            for from_curr in currency_list:                             # First Loop for each currency
+                for to_curr in currency_list:                           # Second Loop for each currency that
 
-                            if dist_table[from_curr] + self.get_weight(from_curr + to_curr) < dist_table[to_curr]:    # negative cycle detected
-                                dist_table[to_curr] = float('-inf')                                                   # update distance table with shorter path distance
-                                pre_table[from_curr] = to_curr                                                        # update predecessor table with shorter path node
-                                updated = True
-
-                iteration = iteration + 1
-
-            # Display the arbitrage opportunity using the predecessors table
-            return show_negative_weight_cycle(pre_table, start)
+                    if dist_table[from_curr] + self.get_weight(from_curr + to_curr) < dist_table[to_curr]:    # negative cycle detected
+                        # Display the arbitrage opportunity using the predecessors table
+                        return show_negative_weight_cycle(pre_table, start)
 
         # No negative cycles detected
         return None
@@ -107,7 +109,9 @@ class weighted_graph:
     def show_arbitrage_opportunities(self):
 
         arbitrage_opportunities = []
+        print ('START' + str(self.currencies))
         for curr in self.currencies:
+            print ('-----------------------' + curr)
             arbitrage = self.bellmanford(curr)
             print (arbitrage)
             if arbitrage is not None and arbitrage not in arbitrage_opportunities:      # if a new potential arbitrage opportunity exists
@@ -131,5 +135,5 @@ class weighted_graph:
 
 
 
-#test = weighted_graph(['USD', 'CAD', 'GBP'])#, 'JPY', 'AUD'])
-#arb = test.show_arbitrage_opportunities()
+# test = weighted_graph(['USD', 'CAD', 'GBP'])#, 'JPY', 'AUD'])
+# arb = test.show_arbitrage_opportunities()
